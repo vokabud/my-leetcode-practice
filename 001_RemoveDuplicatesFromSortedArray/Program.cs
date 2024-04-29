5f4788397595b1332bf576c6ec7a3d4e1433b1ce
vokabud <o.dubakov@gmail.com>
2024-04-29 11:50:38 +0300

    Solve Remove Duplicates From Sorted Array

---
+++ 001_RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _001_RemoveDuplicatesFromSortedArray;
+
+var result = new Solution().RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
+
+Console.WriteLine(result);
---
+++ 001_RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace _001_RemoveDuplicatesFromSortedArray;
+
+internal class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        var count = 0;
+        var index = 0;
+
+        for (var compareIndex = 1; compareIndex < nums.Length; compareIndex++)
+        {
+            if (nums[index] == nums[compareIndex])
+            {
+                continue;
+            }
+
+            index++;
+            nums[index] = nums[compareIndex];
+            count++;
+        }
+        return index + 1;
+    }
+}
