46b934db277e7f2b1c670ecdc470aae8c9efce18
vokabud <o.dubakov@gmail.com>
2024-05-01 16:07:39 +0300

    Solve Contains Duplicate with QuickSort

--- TopInterviewQuestions/L004_ContainsDuplicate.cs
+++ TopInterviewQuestions/L004_ContainsDuplicate.cs
@@ -5,69 +5,78 @@ namespace TopInterviewQuestions;
 // Given an integer array nums, return true if any value appears
 // at least twice in the array, and return false if every element is distinct.
 
-public class Node
-{
-    public int value;
-    public Node left;
-    public Node right;
-}
-
 public class L004_ContainsDuplicate
 {
     public bool ContainsDuplicate(int[] nums)
     {
-        if (nums.Length == 0 || nums.Length == 1)
+        var duplicateNoticed = QuickSort(nums, 0, nums.Length - 1);
+
+        if (duplicateNoticed)
         {
-            return false;
+            return true;
         }
 
-        Node root = new Node
+        for (int i = 0; i < nums.Length - 1; i++)
         {
-            value = nums[0]
-        };
-
-        for (int i = 1; i < nums.Length; i++)
-        {
-            var inserted = InsertInTree(root, nums[i]);
-
-            if (!inserted)
+            if (nums[i] == nums[i + 1])
             {
                 return true;
             }
         }
+
         return false;
     }
 
-    private bool InsertInTree(Node root, int value)
+    public bool QuickSort(int[] array, int leftIndex, int rightIndex)
     {
-        if (root.value == value)
+        var i = leftIndex;
+        var j = rightIndex;
+
+        var pivot = array[leftIndex];
+
+        while (i <= j)
         {
-            return false;
+            while (array[i] < pivot)
+            {
+                if (array[i] == pivot)
+                {
+                    return true;
+                }
+
+                i++;
+            }
+
+            while (array[j] > pivot)
+            {
+                if (array[j] == pivot)
+                {
+                    return true;
+                }
+
+                j--;
+            }
+
+            if (i <= j)
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
         }
 
-        if (value < root.value)
+        if (leftIndex < j)
         {
-            if (root.left == null)
-            {
-                root.left = new Node { value = value };
-                return true;
-            }
-            else
-            {
-                return InsertInTree(root.left, value);
-            }
+
+            QuickSort(array, leftIndex, j);
         }
-        else
+
+        if (i < rightIndex)
         {
-            if (root.right == null)
-            {
-                root.right = new Node { value = value };
-                return true;
-            }
-            else
-            {
-                return InsertInTree(root.right, value);
-            }
+            QuickSort(array, i, rightIndex);
         }
+
+        return false;
     }
 }
