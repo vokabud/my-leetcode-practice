b1fa63454bacf8dc31a7a773cb2ab110192dfbca
vokabud <o.dubakov@gmail.com>
2025-06-01 21:58:10 +0300

    Climbing Stairs riddle

---
+++ DynamicProgramming/DP002_ClimbingStairs.cs
@@ -0,0 +1,16 @@
+﻿namespace DynamicProgramming;
+
+/*
+1 => 1
+2 => 1 1, 2
+3 => 1 1 1, 1 2, 2 1
+4 => 1 1 1 1, 1 1 2, 1 2 1, 2 1 1, 2 2
+*/
+
+public class DP002_ClimbingStairs
+{
+    public int ClimbStairs(int n)
+    {
+        return 0;
+    }
+}
